2adfdfa0647578919ef5044256339daa7d1b21a9
oTAETAEo <ggg7515@naver.com>
2026-06-19 19:02:42 +0900

    Initial commit

---
+++ src/main/java/com/example/aisocket/AisocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.aisocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisocketApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/aisocket/tokenizer/ChatMessage.java
@@ -0,0 +1,5 @@
+package com.example.aisocket.tokenizer;
+
+public record ChatMessage(
+        String message
+) {}
