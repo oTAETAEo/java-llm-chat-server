00a0b37fe6ac6ac1fdefca6adb990060c09e0e1e
oTAETAEo <ggg7515@naver.com>
2026-06-19 19:14:21 +0900

    feat: 채팅 메시지 정보 저장 클래스 추가

--- src/main/java/com/example/aisocket/tokenizer/ChatMessage.java
+++ src/main/java/com/example/aisocket/tokenizer/ChatMessage.java
@@ -1,5 +1,24 @@
 package com.example.aisocket.tokenizer;
 
 public record ChatMessage(
-        String message
-) {}
+        Role role,
+        String content,
+        int tokenCount
+) {
+
+    public ChatMessage {
+
+        if (role == null) {
+            throw new IllegalArgumentException("role은 null이 아니어야 합니다.");
+        }
+
+        if (content == null || content.isBlank()) {
+            throw new IllegalArgumentException("content는 비어있을 수 없습니다.");
+        }
+
+        if (tokenCount < 0) {
+            throw new IllegalArgumentException("tokenCount의 개수는 음수일 수 없습니다.");
+        }
+    }
+
+}
