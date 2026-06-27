a18d16deb7f2ed9a0c933dc43afd9260cb394082
oTAETAEo <ggg7515@naver.com>
2026-06-28 01:33:30 +0900

    refactor: Path 객체 사용으로 NIO 기능 적용

--- src/main/java/com/example/aisocket/week2/adapter/out/persistence/DiskLogRepository.java
+++ src/main/java/com/example/aisocket/week2/adapter/out/persistence/DiskLogRepository.java
@@ -6,19 +6,20 @@ import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 
 public class DiskLogRepository implements LogRepository {
 
     private final BufferedOutputStream bos;
 
-    public DiskLogRepository(String logFilePath) throws IOException {
-        this.bos = new BufferedOutputStream(new FileOutputStream(logFilePath, true), 8192);
+    public DiskLogRepository(Path filePath) throws IOException {
+        this.bos = new BufferedOutputStream(new FileOutputStream(filePath.toFile(), true));
     }
 
     @Override
     public void write(String logMessage) throws IOException {
-        bos.write(logMessage.getBytes(StandardCharsets.UTF_8));
-        bos.write("\n".getBytes(StandardCharsets.UTF_8));
+        String timedLog = "[" + java.time.LocalDateTime.now() + "] " + logMessage + "\n";
+        bos.write(timedLog.getBytes(StandardCharsets.UTF_8));
     }
 
     @Override
