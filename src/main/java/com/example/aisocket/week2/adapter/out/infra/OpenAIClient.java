c10022aa7012ef553b89f8e0ddc59ea94880f0c6
oTAETAEo <ggg7515@naver.com>
2026-06-28 02:23:37 +0900

    chore: sleep 시간 단축

--- src/main/java/com/example/aisocket/week2/adapter/out/infra/OpenAIClient.java
+++ src/main/java/com/example/aisocket/week2/adapter/out/infra/OpenAIClient.java
@@ -8,7 +8,8 @@ public class OpenAIClient implements AiClient{
     @Override
     public String generateResponse(String prompt) throws InterruptedException {
 
-        Thread.sleep(10000);
+//        Thread.sleep(10000);
+        Thread.sleep(1000);
 
         return "[OpenAI GPT-4o] " + prompt + "에 대한 인공지능 분석 답변입니다.";
     }
