99cb7dedc512e50fc916aa0152eb7e4bbcace548
oTAETAEo <ggg7515@naver.com>
2026-08-09 14:43:58 +0900

    chore : 줄 정리

--- src/main/java/com/example/aisocket/project/adapter/in/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/aisocket/project/adapter/in/security/JwtAuthenticationFilter.java
@@ -52,10 +52,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
-    ) throws ServletException, IOException {
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         Optional<String> accessToken = extractAccessToken(request);
         if (accessToken.isEmpty()) {
